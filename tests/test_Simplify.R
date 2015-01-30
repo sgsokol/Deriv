0b328b47f63b34430887d0f26245ecb9e4c090dd
Serguei Sokol <sokol at insa-toulouse.fr>
2015-01-30 19:05:52 +0100

    added numerical term tests; added negative tests;

--- tests/test_Simplify.R
+++ tests/test_Simplify.R
@@ -14,9 +14,18 @@ expect_equal_lang=function(t, r) {
 test_that("rational simplifications", {
    expect_equal_lang(a*b, a*b) # no change must occur
    expect_equal_lang(a/a, 1) # complete single simplification
+   expect_equal_lang(2/2, 1) # complete single numeric simplification
    expect_equal_lang(a*b/(b*a), 1) # complete multiple simplification
    expect_equal_lang(a/(b*a), 1/b) # non complete single simplification in denominator
+   expect_equal_lang(-a/(b*a), -1/b) # non complete single negative simplification in denominator
+   expect_equal_lang(2/(b*2), 1/b) # non complete single numeric simplification in denominator
+   expect_equal_lang(-2/(b*2), -1/b) # non complete single negative numeric simplification in denominator
    expect_equal_lang((a*b)/b, a) # non complete single simplification in numerator
+   expect_equal_lang((a*b)/-b, -a) # non complete single simplification in numerator
+   expect_equal_lang((a*2)/2, a) # non complete single numeric simplification in numerator
+   expect_equal_lang((a*2)/-2, -a) # non complete single negative numeric simplification in numerator
    expect_equal_lang(a*c/(c*b*a), 1/b) # non complete multiple simplification (denominator)
+   expect_equal_lang(a*-c/(c*b*a), -1/b) # non complete multiple negative simplification (denominator)
    expect_equal_lang((a*c*b)/(c*a), b) # non complete multiple simplification (numerator)
+   expect_equal_lang((-a*c*b)/(c*a), -b) # non complete multiple negative simplification (numerator)
 })
